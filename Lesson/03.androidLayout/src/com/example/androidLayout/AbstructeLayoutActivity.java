363ba180a35b737fe07ad227567445df51243d0c
Administrator <wanghaomspace@gmail.com>
2013-08-28 00:21:33 -0700

    03.android layout

---
+++ Lesson/03.androidLayout/src/com/example/androidLayout/AbstructeLayoutActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 8/28/13
+ * Time: 12:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstructeLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.abstractlayout);
+    }
+}
